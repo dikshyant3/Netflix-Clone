76d42a1cfb098a6ffdef8f2c6df8f255546a01e0
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2024-01-04 14:44:15 +0545

    added env file to gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className='text-4xl text-green-500'>
-        Netflix Clone
-    </main>
+  <>
+    <h1 className='text-4xl text-green-500'>
+      Netflix Clone
+    </h1>
+    {/* <button type="button" onClick={()=>signOut()} className='h-10 bg-white text-black'>Logout</button> */}
+  </>
+
   )
 }
