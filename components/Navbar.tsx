c370d66044f59f8b3bf928f11ee468757fd706fa
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2024-01-08 19:04:13 +0545

    NavbarItems added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import NavItem from './NavItem'
+import NavbarItem from './NavbarItem'
 
-type Props = {}
 
-const Navbar = (props: Props) => {
-  return (
-    <nav className='w-full fixed z-40'>
-        <div className='flex items-center px-4 md:px-16 py-6 transition duration-500 bg-zinc-900 bg-opacity-90'>
-            <img className="h-4 lg:h-7" src="/images/logo.png" alt="Logo" />
-            <div className=" flex ml-7 gap-7 hidden lg-flex">
-                <NavItem/>
+const Navbar = () => {
+    return (
+        <nav className='w-full fixed z-40'>
+            <div className='flex items-center px-4 md:px-16 py-6 transition duration-500 bg-zinc-900 bg-opacity-90'>
+                <img className="h-4 lg:h-7" src="/images/logo.png" alt="Logo" />
+                <div className=" flex-row ml-7 gap-7 md:hidden lg:flex">
+                    <NavbarItem label='Home' />
+                    <NavbarItem label='Series' />
+                    <NavbarItem label='Films' />
+                    <NavbarItem label='New & Popular' />
+                    <NavbarItem label='My List' />
+                    <NavbarItem label='Browse by Languages' />
+                </div>
             </div>
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
