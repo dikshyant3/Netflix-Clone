2c10758620168da01d6cd86995f653bc0baf4a34
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2024-01-10 19:24:54 +0545

    Mobile menu almost completed

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
-type Props = {}
+interface MobileMenuProps {
+    visible: boolean;
+}
 
-const MobileMenu = (props: Props) => {
-  return (
-    <div>MobileMenu</div>
-  )
+const MobileMenu = ({visible}:MobileMenuProps) => {
+    return (
+        <div className='bg-black absolute w-48 top-8 left-0 py-5 flex flex-col border-2 border-gray-800'>
+            <div className="flex flex-col gap-4">
+                <div className="px-3 text-center text-white hover:underline">
+                    Home
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default MobileMenu
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+"use client"
+
+import React, { useCallback, useState } from 'react'
 import NavbarItem from './NavbarItem'
 import { BsChevronDown } from "react-icons/bs";
 import MobileMenu from './MobileMenu';
 
 
-
-
 const Navbar = () => {
+    const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+    const toggleMobileMenu = useCallback(() => {
+        setShowMobileMenu((current) => !current)
+    }, [])
     return (
         <nav className='w-full fixed z-40'>
             <div className='flex items-center px-4 md:px-16 py-6 transition duration-500 bg-zinc-900 bg-opacity-90'>
@@ -19,12 +24,12 @@ const Navbar = () => {
                     <NavbarItem label='My List' />
                     <NavbarItem label='Browse by Languages' />
                 </div>
-                <div className="flex items-center gap-2 ml-8 cursor-pointer lg:hidden ">
+                <div onClick={toggleMobileMenu} className="flex items-center gap-2 ml-8 cursor-pointer relative lg:hidden ">
                     <p className="text-white text-sm">
                         Browse
                     </p>
-                    <BsChevronDown className="text-white transition"/>
-                    <MobileMenu/>
+                    <BsChevronDown className="text-white transition" />
+                    <MobileMenu visible={showMobileMenu} />
                 </div>
             </div>
         </nav>
