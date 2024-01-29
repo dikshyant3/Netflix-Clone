8d88cdbe1121842c9af450055d175ea6861a257d
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2024-01-29 16:33:01 +0545

    adding some check

--- app/api/current/route.ts
+++ app/api/current/route.ts
@@ -3,8 +3,17 @@ import { authOptions } from '../auth/[...nextauth]/route';
 
 
 export async function GET() {
+  try{
   const session = await getServerSession(authOptions)
+
+  if(!session){
+    return Response.json({status:401},"Unauthorized")
+  }
 
  
   return Response.json( {user:session?.user} )
+}
+catch(error){
+  Response.json({status:401},"Internal Server Error")
+}
 }
