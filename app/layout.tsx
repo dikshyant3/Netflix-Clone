004c30aa032ffe85c7f04a1c0b860ad373b00c70
dikshyantdhungana <dhunganadikshyant12@gmail.com>
2024-02-02 09:45:24 +0545

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,11 @@ export default function RootLayout({
   return (
     <>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <link rel="icon" href="/images/favicon.ico" sizes="any" />
+
+          {children}
+        </body>
       </html>
     </>
   )
